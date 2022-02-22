e8465451de06996096f238415ad87a747eb05448
AdilMahmoudRion <adilmahmoudrion@gmail.com>
2022-02-23 04:01:26 +0600

    first commit

---
+++ src/Components/Home/Home/Home.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Login from "../../Authentication/Login/Login";
+import Registration from "../../Authentication/Registration/Registration";
+
+const Home = () => {
+  return (
+    <div>
+      <Login></Login>
+    </div>
+  );
+};
+
+export default Home;
